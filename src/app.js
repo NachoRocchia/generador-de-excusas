d2e1e949189768059251573854a145b7c7cfa8d5
NachoRocchia <96082282+NachoRocchia@users.noreply.github.com>
2025-10-29 23:35:30 +0000

    generador de excusas ignacio rocchia

--- src/app.js
+++ src/app.js
@@ -5,7 +5,16 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ['Mi perro', 'Mi abuela', 'El cartero', 'el pajaro'];
+let action = ['se comio', 'me quito', 'aplasto', 'rompio'];
+let what = ['la tarea', 'mi telefono', 'el auto'];
+let when = ['antes de clase', 'cuando estaba durmiendo', 'cuando estaba entrenando', 'mientras alrmorzaba', 'cuando estaba en misa'];
+
+const randomizador = (anyArray) => { 
+  let random = Math.floor(Math.random() * 3);
+  return anyArray[random]
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = randomizador(who) + " " + randomizador(action) + " " + randomizador(what) + " " + randomizador(when)
 };
